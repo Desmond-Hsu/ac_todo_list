6c3636e875204586e9186b461bb67c91968cd081
Desmond-Hsu <derek97072002@gmail.com>
2021-10-11 13:10:01 +0800

    feat: model todo add idDone params

--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,10 @@ const todoSchema = new Schema({
   name: {
     type: String,//資料型別為字串
     required: true,//必填欄位
+  },
+  isDone: {
+    type:Boolean,
+    default:false
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
